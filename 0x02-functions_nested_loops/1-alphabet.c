02bd3f6dad5eaaa9c415d3ad5aeaeeaf3cedaa76
mariaam <mariaammalaak846@gmail.com>
2023-06-21 11:22:09 -0700

    this is my file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always o.
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define main_h
 #include <stdio.h>
 int _putchar(char);
+void print_alphabet(void);
 #endif
